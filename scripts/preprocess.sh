c63c2c85aaa713ad92b6c730bf026968f411f782
jeniferleleany.meyer <jeniferleleany.meyer@uzh.ch>
2024-05-13 14:43:15 +0200

    changes

---
+++ scripts/preprocess.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+scripts=$(dirname "$0")
+base=$scripts/..
+
+source_input=$base/data/train.it-en.it
+source_output=$base/data/train.it
+
+target_input=$base/data/train.it-en.en
+target_output=$base/data/train.en
+
+head -n 100000 $source_input > $source_output
+head -n 100000 $target_input > $target_output
+
+echo "cut off first 100k sentences done"
