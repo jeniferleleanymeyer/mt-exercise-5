726e238f63804968611cce31e2ad086d6fa84eb1
jeniferleleany.meyer <jeniferleleany.meyer@uzh.ch>
2024-05-13 13:37:22 +0200

    changes

---
+++ scripts/train_model_1.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+scripts=$(dirname "$0")
+base=$scripts/..
+
+models=$base/models
+configs=$base/configs
+
+mkdir -p $models
+
+# changed
+num_threads=10
+
+# measure time
+
+SECONDS=0
+
+logs=$base/logs
+
+#changed
+model_name=transformer_model_1
+
+mkdir -p $logs
+
+mkdir -p $logs/$model_name
+
+OMP_NUM_THREADS=$num_threads python3 -m joeynmt train $configs/$model_name.yaml > $logs/$model_name/out 2> $logs/$model_name/err
+
+echo "time taken:"
+echo "$SECONDS seconds"
